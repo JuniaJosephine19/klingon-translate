2b339b7ae8d806b9914e94868549df826f3fdece
Junia Josephine <junia.josephine@gmail.com>
2021-09-21 01:17:44 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var btnTranslator= document.querySelector("#btn-trans");
+var inputText= document.querySelector("#in-txt-area");
+var outputText= document.querySelector("#out-txt-area");
+
+var url = "https://api.funtranslations.com/translate/klingon";
+
+function constructUrl(text){
+    return url + "?text=" + text;
+}
+
+function errorHandler(error){
+    console.log("Error occured! " + error);
+    alert("Error occured! Something went wrong!");
+}
+
+function getTranslation(input){
+    var inputTxt = input.value;
+    fetch(constructUrl(inputTxt))
+    .then(Response => Response.json())
+    .then(json =>{
+        var translatedTxt = json.contents.translated;
+        outputText.innerText = translatedTxt;
+    })
+    .catch(errorHandler);
+}
+
+btnTranslator.addEventListener("click", function clickEventHandler(){
+    getTranslation(inputText);
+})
